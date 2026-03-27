9dd9b9b21b47ac4b7a2387fc73c45fd030057038
Carlos Eduardo Roseno Paiva <carloseduardo.paiva48@gmail.com>
2026-03-27 09:02:54 -0300

    Adicionando os arquivos feitos em sala

--- praticas/pratica02/app-atividade-aula-tarefas/App.js
+++ praticas/pratica02/app-atividade-aula-tarefas/App.js
@@ -1,42 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
-
-import { 
-  rotulo_btn_cadastro_meta, 
-  rotulo_lista_metas, 
-  rotulo_input_meta 
-} from './mensagens';
-
+import { StyleSheet, View } from 'react-native';
 import { useState } from 'react';
-
 import MetasList from './components/MetasList';
+import MetaInput from './components/MetaInput';
 
 export default function App() {
 
-  const [inputMetaText, setInputMetaText] = useState('');
   const [metas, setMetas] = useState([]);
 
-  function metaInputHandler(inputText){
-    setInputMetaText(inputText)
-  }
-
-  function adicionarMetaHandler(){
-    setMetas([...metas, inputMetaText]);
+  function adicionarMetaHandler(inputMeta) {
+    setMetas([...metas, inputMeta]);
   }
 
   return (
     <View style={styles.mainContainer}>
-      <View style={{  flexDirection: 'row', justifyContent: 'space-between', flex:1 }}>
-        <View style={{width: '65%'}}>
-          <TextInput style={styles.inputText} 
-          onChangeText={metaInputHandler}
-          placeholder={rotulo_input_meta} />
-        </View>
-        <View style={{width:'30%'}}>
-          <Button title={rotulo_btn_cadastro_meta}
-          onPress={adicionarMetaHandler} />
-        </View>
-      </View>
+      <MetaInput onAddMeta={adicionarMetaHandler} />
       <View style={styles.metaContainer}>
         <MetasList array={metas} />
       </View>
@@ -45,18 +22,10 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   mainContainer: {
     padding: 30,
     flex: 1,
     flexDirection: 'column',
-  },
-  inputText: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
   },
   metaContainer: {
     flex: 15,
